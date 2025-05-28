2e36adfaa8fa475e362b2806f257bfb4ba163f61
MiranaM <0resama@mail.ru>
2025-05-28 13:19:24 +0300

    second (1)

--- Makefile
+++ Makefile
@@ -1,4 +1,16 @@
 PORT ?= 8000
 
+setup:
+	mkdir -p templates/layouts
+	cp -n templates/layouts/base.phtml || true
+	cp -n templates/index.phtml || true
+
+install:
+	composer install
+	make setup
+
 start:
 	PHP_CLI_SERVER_WORKERS=5 php -S 0.0.0.0:$(PORT) -t public
+
+lint:
+	composer exec --verbose phpcs public templates --standard=PSR12
--- public/index.php
+++ public/index.php
@@ -1,16 +1,33 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-use Slim\Factory\AppFactory;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
+
+require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
+// Настройка шаблонизатора
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+
+// Главная страница
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello from Slim!");
-    return $response;
+    $renderer = new PhpRenderer(__DIR__ . '/../templates');
+    
+    // Рендерим контент страницы
+    $content = $renderer->fetch('index.phtml', [
+        'title' => 'Анализатор страниц',
+        'description' => 'Бесплатно проверяйте сайты на SEO-пригодность'
+    ]);
+    
+    // Рендерим layout с контентом
+    return $renderer->render($response, 'layouts/base.phtml', [
+        'title' => 'Анализатор страниц',
+        'content' => $content
+    ]);
 });
 
 $app->run();
