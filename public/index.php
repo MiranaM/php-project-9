e490c291529aeec4403829e33f354bcef5b088a3
MiranaM <0resama@mail.ru>
2025-05-28 15:59:07 +0300

    second (4)

--- public/index.php
+++ public/index.php
@@ -1,33 +1,17 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+require __DIR__ . '/../vendor/autoload.php';
+
 use Slim\Factory\AppFactory;
 use Slim\Views\PhpRenderer;
 
-require __DIR__ . '/../vendor/autoload.php';
-
 $app = AppFactory::create();
 
-// Настройка шаблонизатора
-$app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, true, true);
+// Шаблонизатор указывает папку с шаблонами
+$renderer = new PhpRenderer(__DIR__ . '/../templates');
 
-// Главная страница
-$app->get('/', function (Request $request, Response $response) {
-    $renderer = new PhpRenderer(__DIR__ . '/../templates');
-
-    // Рендерим контент страницы
-    $content = $renderer->fetch('index.phtml', [
-        'title' => 'Анализатор страниц',
-        'description' => 'Бесплатно проверяйте сайты на SEO-пригодность'
-    ]);
-
-    // Рендерим layout с контентом
-    return $renderer->render($response, 'layouts/base.phtml', [
-        'title' => 'Анализатор страниц',
-        'content' => $content
-    ]);
+$app->get('/', function ($request, $response) use ($renderer) {
+    return $renderer->render($response, 'home.phtml');
 });
 
 $app->run();
