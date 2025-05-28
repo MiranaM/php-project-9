830db73c87c36d6342275be606a72fc3b5db8014
MiranaM <0resama@mail.ru>
2025-05-28 13:26:40 +0300

    second (2)

--- public/index.php
+++ public/index.php
@@ -16,13 +16,13 @@ $app->addErrorMiddleware(true, true, true);
 // Главная страница
 $app->get('/', function (Request $request, Response $response) {
     $renderer = new PhpRenderer(__DIR__ . '/../templates');
-    
+
     // Рендерим контент страницы
     $content = $renderer->fetch('index.phtml', [
         'title' => 'Анализатор страниц',
         'description' => 'Бесплатно проверяйте сайты на SEO-пригодность'
     ]);
-    
+
     // Рендерим layout с контентом
     return $renderer->render($response, 'layouts/base.phtml', [
         'title' => 'Анализатор страниц',
